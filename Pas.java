c7b0dcd1e72fd9ddad3749e56d91b3a86ac1e5dd
PhanterJR <junior.conex@gmail.com>
2017-05-16 04:25:09 -0300

    Update Pas.java

--- Pas.java
+++ Pas.java
@@ -6,6 +6,11 @@ public class Pas{ //Nome da classe de nosso trabalho
         Boolean sair_programa=false;
         String menu;
         int opcao_int = 0;
+        int total_ocorrencias=0;
+        String reg_ocorrencias[] = new String[500];
+        String reg_locais[] = new String[500];
+        String reg_descricoes[] = new String[500];
+        String reg_envolvidos[] = new String[500];
         do{ //pagina 151 do {//bloco de comandos}while (expressão lógica)
             String ocorrencia_atual = "";
             String local_atual="";
@@ -124,33 +129,64 @@ public class Pas{ //Nome da classe de nosso trabalho
                                 }   
                             } while (cont<9);
                         } else{
-                            texto_caso_1="<html><i><b>OCORRÊNCIA ADICIONADA! - RESUMO</b></i><br><br>"+
+
+                            
+                            texto_caso_1="<html><i><b>OCORRÊNCIA "+(total_ocorrencias+1)+" ADICIONADA! - RESUMO</b></i><br><br>"+
                             "OCORRENCIA: "+ocorrencia_atual+"<br>"+
                             "LOCAL: "+local_atual+"<br>"+
                             "DESCRICAO: "+descricao_atual+"<br>"+
                             "ENVOLVIDOS: "+envolvido_atual+"<br><br>"+
                             "==============================================================<br>"+
-                            "O QUER FAZER AGORA? DIGITE 1 PARA ADICIONAR OUTRA OCORRÊNCIA<br>Outra opção digitada irá voltar ao Menu principal</html>";
+                            "1 - SALVAR E ADICIONAR NOVA OCORRÊNCIA<br>"+
+                            "2 - APAGAR E ADICIONAR NOVA OCORRÊNCIA<br>"+
+                            "3 - SALVAR E VOLTAR AO MENU PRINCIPAL<br>"+
+                            "9 - APAGAR E VOLTAR AO MENU PRINCIPAL<br>"+
+                            "==============================================================<br>"+
+                            "O QUER FAZER AGORA?</html>";
                             String opcao_2=JOptionPane.showInputDialog(texto_caso_1);
                             switch (opcao_2) {
                                 case "1":
+                                    reg_ocorrencias[total_ocorrencias]=ocorrencia_atual;
+                                    reg_locais[total_ocorrencias]=local_atual;
+                                    reg_descricoes[total_ocorrencias]=descricao_atual;
+                                    reg_envolvidos[total_ocorrencias]=envolvido_atual;
+                                    total_ocorrencias++;
                                     ocorrencia_atual = "";
                                     local_atual="";
                                     descricao_atual="";
                                     envolvido_atual="";
                                     envolvidos = new String[10];
-                                break;
-                                default:
-                                    System.out.println(ocorrencia_atual);
+                                    break;
+                                case "2":
+                                    ocorrencia_atual = "";
+                                    local_atual="";
+                                    descricao_atual="";
+                                    envolvido_atual="";
+                                    envolvidos = new String[10];
+                                    break;
+                                case "3":
+                                    reg_ocorrencias[total_ocorrencias]=ocorrencia_atual;
+                                    reg_locais[total_ocorrencias]=local_atual;
+                                    reg_descricoes[total_ocorrencias]=descricao_atual;
+                                    reg_envolvidos[total_ocorrencias]=envolvido_atual;
+                                    total_ocorrencias++;
                                     sair_caso_1=true;
+                                    break;
+                                case "9":
+                                    ocorrencia_atual = "";
+                                    local_atual="";
+                                    descricao_atual="";
+                                    envolvido_atual="";
+                                    envolvidos = new String[10];
+                                    sair_caso_1=true;
+                                    break;
+                                default:
+                                    JOptionPane.showMessageDialog(null, "Opção inválida"); //Não tem no livro, porém poderia colocar "System.out.ln("Opção Inválida);"
                                 break;
                             }
                         };
-
                     }while(sair_caso_1==false);
-
-                    break;
-                
+                    break;              
                 case "2":
                     
                     break;
@@ -170,9 +206,7 @@ public class Pas{ //Nome da classe de nosso trabalho
                 default:
                     JOptionPane.showMessageDialog(null, "Opção inválida"); //Não tem no livro, porém poderia colocar "System.out.ln("Opção Inválida);"
                     break;
-
             }
-
         }while(sair_programa==false);
 
 
