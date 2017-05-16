3d43b235d1816388f6ace19ba7b0dbeef737c7e6
PhanterJR <junior.conex@gmail.com>
2017-05-16 19:39:39 -0300

    Update Pas.java

--- Pas.java
+++ Pas.java
@@ -4,6 +4,7 @@ public class Pas{ //Nome da classe de nosso trabalho
 
     public static void main(String[] args) { //Este é o método Main
         Boolean sair_programa=false;
+        Boolean sair_ordem_ocorrencia;
         String menu;
         int opcao_int = 0;
         int total_ocorrencias=0;
@@ -11,6 +12,85 @@ public class Pas{ //Nome da classe de nosso trabalho
         String reg_locais[] = new String[500];
         String reg_descricoes[] = new String[500];
         String reg_envolvidos[] = new String[500];
+        String vfiltro1[];
+        int vfiltro2[];
+        int rank1[] = new int[5];
+        int rank2[] = new int[5];
+
+        reg_ocorrencias[0]="Preconceito Social";
+        reg_ocorrencias[1]="Preconceito Social";
+        reg_ocorrencias[2]="Intolerância Religiosa";
+        reg_ocorrencias[3]="Preconceito Social";
+        reg_ocorrencias[4]="Bulling";
+        reg_ocorrencias[5]="Preconceito Social";
+        reg_ocorrencias[6]="Gênero";
+        reg_ocorrencias[7]="Bulling";
+        reg_ocorrencias[9]="Intolerância Religiosa";
+        reg_ocorrencias[8]="Desrespeito ao Idoso";
+        reg_ocorrencias[9]="Intolerância Religiosa";
+        reg_ocorrencias[10]="Preconceito Social";
+        reg_ocorrencias[11]="Intolerância Religiosa";
+        reg_ocorrencias[12]="Preconceito Social";
+        reg_ocorrencias[13]="Preconceito Racial";
+        reg_ocorrencias[13]="Preconceito Racial";
+        reg_ocorrencias[14]="Intolerância Religiosa";
+        reg_ocorrencias[15]="Intolerância Religiosa";
+        reg_locais[0]="Tobias Barreto";
+        reg_locais[1]="Tobias Barreto";
+        reg_locais[2]="Lagarto";
+        reg_locais[3]="Itabaiana";
+        reg_locais[4]="Aracaju";
+        reg_locais[5]="Aracaju";
+        reg_locais[6]="Tobias Barreto";
+        reg_locais[7]="Lagarto";
+        reg_locais[9]="Itabaiana";
+        reg_locais[8]="Lagarto";
+        reg_locais[9]="Aracaju";
+        reg_locais[10]="Aracaju";
+        reg_locais[11]="Tobias Barreto";
+        reg_locais[12]="Lagarto";
+        reg_locais[13]="Itabaiana";
+        reg_locais[13]="Preconceito Racial";
+        reg_locais[14]="Umbaúba";
+        reg_locais[15]="Tobias Barreto";
+        reg_descricoes[0]="Sem descrição";
+        reg_descricoes[1]="Sem descrição";
+        reg_descricoes[2]="Resolvido";
+        reg_descricoes[3]="Sem descrição";
+        reg_descricoes[4]="Em processo";
+        reg_descricoes[5]="Em processo";
+        reg_descricoes[6]="Estaca zero";
+        reg_descricoes[7]="Emcaminhado ao Juiz";
+        reg_descricoes[9]="Sem descrição";
+        reg_descricoes[8]="Resolvido";
+        reg_descricoes[9]="Em processo";
+        reg_descricoes[10]="Em processo";
+        reg_descricoes[11]="Estaca zero";
+        reg_descricoes[12]="Testemunhas a ser encontrada";
+        reg_descricoes[13]="Sem descrição";
+        reg_descricoes[13]="Emcaminhado ao Juiz";
+        reg_descricoes[14]="Emcaminhado ao Juiz";
+        reg_descricoes[15]="Emcaminhado ao Juiz";
+        reg_envolvidos[0]="Pedro, Simão, Jackson";
+        reg_envolvidos[1]="João, José, Patrícia";
+        reg_envolvidos[2]="José, Maria, Pedro";
+        reg_envolvidos[3]="Marcia, Marcos";
+        reg_envolvidos[4]="Maiza, Viana";
+        reg_envolvidos[5]="Viana, Souzinha";
+        reg_envolvidos[6]="Marcos, José, Maurício";
+        reg_envolvidos[7]="Bulling";
+        reg_envolvidos[9]="Marcondes, João, José";
+        reg_envolvidos[8]="Santos, Manoel";
+        reg_envolvidos[9]="Marcondes, João";
+        reg_envolvidos[10]="Maria, Pedro";
+        reg_envolvidos[11]="Marcondes, João, Maria";
+        reg_envolvidos[12]="Maria, Pedro, Victor";
+        reg_envolvidos[13]="Maria, Maurício";
+        reg_envolvidos[13]="Maria, Maurício";
+        reg_envolvidos[14]="Cirano, Viana, Rosalia";
+        reg_envolvidos[15]="Ingrid, Marcelo, Hélio";
+
+
         do{ //pagina 151 do {//bloco de comandos}while (expressão lógica)
             total_ocorrencias=0;
             for (int y=0;y<500 ;y++ ) {
@@ -105,7 +185,7 @@ public class Pas{ //Nome da classe de nosso trabalho
                                             System.out.println("Foi vazio");
                                         break;
                                         case ".":
-                                            System.out.println("interrompendo loop");
+                                            System.out.println("Interrompendo loop");
                                             cont=10;
                                             envolvido_atual=envolvido_atual+".";
                                         default:
@@ -137,8 +217,6 @@ public class Pas{ //Nome da classe de nosso trabalho
                                 }   
                             } while (cont<9);
                         } else{
-
-                            
                             texto_caso_1="<html><i><b>OCORRÊNCIA "+(total_ocorrencias+1)+" ADICIONADA! - RESUMO</b></i><br><br>"+
                             "OCORRENCIA: "+ocorrencia_atual+"<br>"+
                             "LOCAL: "+local_atual+"<br>"+
@@ -193,19 +271,202 @@ public class Pas{ //Nome da classe de nosso trabalho
                                 break;
                             }
                         };
-                    }while(sair_caso_1==false);
+                    } while(sair_caso_1==false);
                     break;              
                 case "2":
-                    
+                    vfiltro1 = new String[500];
+                    vfiltro2 = new int[500];
+                    String posicao_atual="";
+                    if ((reg_ocorrencias[0]=="")||(reg_ocorrencias[0]==null)){
+                        JOptionPane.showMessageDialog(null, "Não há ocorrências"); //Não tem no livro, porém poderia colocar "System.out.ln("Não há ocorrencias);"
+                        sair_ordem_ocorrencia=true;
+                    } else {
+                        sair_ordem_ocorrencia=false;
+                        int cont_caso2=0;
+                        do{
+                            posicao_atual=reg_ocorrencias[cont_caso2];
+                            if((posicao_atual=="")||(posicao_atual==null)||(cont_caso2>498)){
+                                sair_ordem_ocorrencia=true;
+                            } else {
+                                for (int z=0; z<499; z++) {
+                                    if ((vfiltro1[z]=="")||(vfiltro1[z]==null)){
+                                        vfiltro1[z]=posicao_atual;
+                                        vfiltro2[z]=1;
+                                        break;
+                                    } else if (vfiltro1[z]==posicao_atual) {
+                                        vfiltro2[z]=vfiltro2[z]+1;
+                                        break;
+                                    }
+                                    
+                                }
+                            }
+                            cont_caso2++;
+
+
+                        }while(sair_ordem_ocorrencia==false);
+                        rank1[0]=0;
+                        rank1[1]=0;
+                        rank1[2]=0;
+                        rank1[3]=0;
+                        rank1[4]=0;
+                        for (int v=0; v<5; v++) {
+                            for (int u=0; u<499; u++){
+                                if ((vfiltro1[u]=="")||(vfiltro1[u]==null)){
+                                    break;
+                                } else{
+                                    if (v==0){
+                                        if (rank1[0]<vfiltro2[u]){
+                                            rank1[0]=vfiltro2[u];
+                                            rank2[0]=u;
+                                        }
+                                    }
+                                    if (v==1){
+                                        if ((rank1[1]<vfiltro2[u]) && (rank2[0]!=u)){
+                                            rank1[1]=vfiltro2[u];
+                                            rank2[1]=u; 
+                                        }
+                                    }
+                                    if (v==2){
+                                        if ((rank1[2]<vfiltro2[u]) && (rank2[0]!=u) && (rank2[1]!=u)){
+                                            rank1[2]=vfiltro2[u];
+                                            rank2[2]=u; 
+                                        }
+                                    }
+                                    if (v==3){
+                                        if ((rank1[3]<vfiltro2[u]) && (rank2[0]!=u) && (rank2[1]!=u) && (rank2[2]!=u)){
+                                            rank1[3]=vfiltro2[u];
+                                            rank2[3]=u; 
+                                        }
+                                    }
+                                    if (v==4){
+                                        if ((rank1[4]<vfiltro2[u]) && (rank2[0]!=u) && (rank2[1]!=u) && (rank2[2]!=u) && (rank2[3]!=u)){
+                                            rank1[4]=vfiltro2[u];
+                                            rank2[4]=u; 
+                                        }
+                                    }
+
+                                }
+                            }
+                            
+                        }
+                        String texto_caso_2="<html><i><b>RELATÓRIO - AS 5 MAIORES OCORRÊNCIAS</b></i><br><br>";
+                                        
+                        for (int r=0; r<5; r++) {
+                            texto_caso_2=texto_caso_2+(r+1)+ " - "+ vfiltro1[rank2[r]]+ " com "+vfiltro2[rank2[r]]+" OCORRÊNCIA(S)<br>";
+                        }
+                        texto_caso_2=texto_caso_2+"==============================================================<br>PRESSIONE ENTER PARA CONTINUAR</html>";
+                        String nada=JOptionPane.showInputDialog(texto_caso_2);             
+                    }
+
+
                     break;
                 case "3":
-                    
+                    String texto_caso_3="";
+                    if ((reg_ocorrencias[0]=="")||(reg_ocorrencias[0]==null)){
+                        JOptionPane.showMessageDialog(null, "Não há registros"); //Não tem no livro, porém poderia colocar "System.out.ln("Não há ocorrencias);"
+  
+                    }else{
+                        texto_caso_3="<html><i><b>OCORRÊNCIAS E SEUS ENVOLVIDOS</b></i><br><br>";
+                        for(int x=0; x<500; x++){
+                            if ((reg_ocorrencias[x]=="")||(reg_ocorrencias[x]==null)){
+                                break;
+                            }else{ 
+                                texto_caso_3=texto_caso_3+"<i>"+(x+1)+" - "+reg_ocorrencias[x]+":</i><br>";
+                                texto_caso_3=texto_caso_3+" > "+reg_envolvidos[x]+"<br><br>";
+                            }
+                        }
+                    }
+                    texto_caso_3=texto_caso_3+"==============================================================<br>PRESSIONE ENTER PARA CONTINUAR</html>";
+                    String nada=JOptionPane.showInputDialog(texto_caso_3);   
                     break;
                 case "4":
+                    vfiltro1 = new String[500];
+                    vfiltro2 = new int[500];
+                    String posicao_atual_c4="";
+                    String texto_caso_4="";
+                    if ((reg_ocorrencias[0]=="")||(reg_ocorrencias[0]==null)){
+                        JOptionPane.showMessageDialog(null, "Não há ocorrências"); //Não tem no livro, porém poderia colocar "System.out.ln("Não há ocorrencias);"
+                        sair_ordem_ocorrencia=true;
+                    } else {
+                        sair_ordem_ocorrencia=false;
+                        int cont_caso2=0;
+                        do{
+                            posicao_atual_c4=reg_ocorrencias[cont_caso2];
+                            if((posicao_atual_c4=="")||(posicao_atual_c4==null)||(cont_caso2>498)){
+                                sair_ordem_ocorrencia=true;
+                            } else {
+                                for (int z=0; z<499; z++) {
+                                    if ((vfiltro1[z]=="")||(vfiltro1[z]==null)){
+                                        vfiltro1[z]=posicao_atual_c4;
+                                        vfiltro2[z]=1;
+                                        break;
+                                    } else if (vfiltro1[z]==posicao_atual_c4) {
+                                        vfiltro2[z]=vfiltro2[z]+1;
+                                        break;
+                                    }
+                                    
+                                }
+                            }
+                            cont_caso2++;
+                        }while(sair_ordem_ocorrencia==false);
+                        texto_caso_4="<html><i><b>OCORRÊNCIAS EM PORCENTAGEM</b></i><br><br>";
+                        for(int x=0; x<500; x++){
+                            if ((vfiltro1[x]=="")||(vfiltro1[x]==null)){
+                                break;
+                            }else{
+                                double porcentagem = (vfiltro2[x]/(double)total_ocorrencias*100.0);
+                                String porcentagem_formatado=String.format("%.2f",porcentagem);
+                                texto_caso_4=texto_caso_4+(x+1)+ " - "+ vfiltro1[x]+ " com "+porcentagem_formatado+"%<br>";
+
+                            }
+                        }
+                    }
+                    texto_caso_4=texto_caso_4+"==============================================================<br>PRESSIONE ENTER PARA CONTINUAR</html>";
+                    String nada4=JOptionPane.showInputDialog(texto_caso_4); 
                     
                     break;
                 case "5":
-                    
+                    vfiltro1 = new String[500];
+                    vfiltro2 = new int[500];
+                    String posicao_atual_c5="";
+                    String texto_caso_5="";
+                    if ((reg_locais[0]=="")||(reg_locais[0]==null)){
+                        JOptionPane.showMessageDialog(null, "Não há ocorrências"); //Não tem no livro, porém poderia colocar "System.out.ln("Não há ocorrencias);"
+                        sair_ordem_ocorrencia=true;
+                    } else {
+                        sair_ordem_ocorrencia=false;
+                        int cont_caso2=0;
+                        do{
+                            posicao_atual_c5=reg_locais[cont_caso2];
+                            if((posicao_atual_c5=="")||(posicao_atual_c5==null)||(cont_caso2>498)){
+                                sair_ordem_ocorrencia=true;
+                            } else {
+                                for (int z=0; z<499; z++) {
+                                    if ((vfiltro1[z]=="")||(vfiltro1[z]==null)){
+                                        vfiltro1[z]=posicao_atual_c5;
+                                        vfiltro2[z]=1;
+                                        break;
+                                    } else if (vfiltro1[z]==posicao_atual_c5) {
+                                        vfiltro2[z]=vfiltro2[z]+1;
+                                        break;
+                                    }
+                                    
+                                }
+                            }
+                            cont_caso2++;
+                        }while(sair_ordem_ocorrencia==false);
+                        texto_caso_5="<html><i><b>LOCAIS E SUAS OCORRÊNCIAS</b></i><br><br>";
+                        for(int x=0; x<500; x++){
+                            if ((vfiltro1[x]=="")||(vfiltro1[x]==null)){
+                                break;
+                            }else{
+                                texto_caso_5=texto_caso_5+(x+1)+ " - "+ vfiltro1[x]+ " com "+vfiltro2[x]+" ocorrencia(s)<br>";
+
+                            }
+                        }
+                    }
+                    texto_caso_5=texto_caso_5+"==============================================================<br>PRESSIONE ENTER PARA CONTINUAR</html>";
+                    String nada5=JOptionPane.showInputDialog(texto_caso_5);                     
                     break;
                 case "9":
                     JOptionPane.showMessageDialog(null, "Programa sendo finalizado"); //Não tem no livro, porém poderia colocar "System.out.ln("Programa sendo finalizado");"
